311ca932a3697665c2a44670a2f7115d6282fa9b
insist <jx_xiaojin@aliyun.com>
2018-05-04 17:48:17 +0800

    add src

---
+++ src/Action.php
@@ -0,0 +1,15 @@
+<?php
+namespace Jinsoft\Xiao;
+/**
+ * Created by PhpStorm.
+ * Author: xiaojin
+ * Created At: 2018/5/4 15:36
+ */
+class Action
+{
+
+    public function say()
+    {
+        return "say";
+    }
+}
